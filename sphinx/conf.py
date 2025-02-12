9f880be5ea5b4d0cdcd00f826020675823f354f3
dw42CSCE <dw42@email.sc.edu>
2025-02-11 23:44:31 -0500

    Fixing main

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -5,7 +5,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
